42edab49c1ace03df8172b30088e32472c6865f0
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-01-31 14:50:18 +0000

    solved

--- contest/abc121/src/bin/c.rs
+++ contest/abc121/src/bin/c.rs
@@ -1,10 +1,33 @@
-use proconio::input;
+use proconio::{derive_readable, fastout, input};
 
+#[derive_readable]
+struct Shop {
+    price: u64,
+    n_stock: u64,
+}
+
+#[fastout]
 fn main() {
-    input!{
-        n: i64,
-        m: i64,
-        
+    input! {
+        n: usize,
+        m: u64,
+        mut shops: [Shop; n]
     }
-    todo!();
+
+    shops.sort_by_key(|s| s.price);
+
+    let mut cost = 0;
+    let mut n_need = m;
+
+    for shop in shops {
+        if n_need > shop.n_stock {
+            cost += shop.price * shop.n_stock;
+            n_need -= shop.n_stock;
+        } else {
+            cost += n_need * shop.price;
+            break;
+        }
+    }
+
+    println!("{}", cost);
 }
