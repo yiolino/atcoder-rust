865f0512831109d776ffb1a94db110d3e4422bbf
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-02-27 08:33:35 +0000

    solved

---
+++ contest/abc155/src/bin/c.rs
@@ -0,0 +1,34 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars};
+#[allow(unused_imports)]
+use std::collections::HashMap;
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input!{
+        N: i64,
+    }
+
+    let mut map = HashMap::new();
+
+    for _ in 0..N {
+        input!{Si: String}
+        *map.entry(Si).or_insert(0) += 1;
+    }
+
+    let (_, max) = &map.iter().max_by_key(|entry| entry.1).unwrap();
+    let mut vec =  vec![];
+
+    for (k, v) in &map {
+        if &v == max {
+            vec.push(k);
+        }
+    }
+
+    vec.sort();
+
+    for v in vec {
+        println!("{}", v);
+    }
+}
