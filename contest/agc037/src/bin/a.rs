e4ffd43c71a47658695f5782eaea4997ef139f85
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-02-28 03:16:08 +0000

    horyu

---
+++ contest/agc037/src/bin/a.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars};
+#[allow(unused_imports)]
+use std::collections::HashMap;
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input!{
+    }
+    println!();
+}
