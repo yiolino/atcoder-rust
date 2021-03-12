67de6a4bcc2437a999c7d1196a0a7d6fb8499284
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-03-12 00:06:43 +0000

    another solution

--- contest/dp/src/bin/a.rs
+++ contest/dp/src/bin/a.rs
@@ -9,20 +9,55 @@ use std::cmp::{max, min};
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        N: i64,
+        N: usize,
         h: [i64; N],
     }
 
-    let mut vec = vec![1000_000_000_000_000; N as usize];
+    let mut dp = vec![std::i64::MAX; N];
 
-    vec[0] = 0;
-    vec[1] = (&h[1] - &h[0]).abs();
-    for i in 2..N {
-        vec[i as usize] = min((&h[i as usize] - &h[(i-1) as usize]).abs() + vec[(i-1) as usize], 
-                                vec[i as usize]);
-        vec[i as usize] = min((&h[i as usize] - &h[(i-2) as usize]).abs() + vec[(i-2) as usize],
-                                vec[i as usize]);
+    println!("{}", rec(N-1, &mut dp, &h));
+}
+
+fn rec(i: usize, dp: &mut Vec<i64>, h: &Vec<i64>) -> i64 {
+    // DPの値が更新されていたらそのままリターン
+    if dp[i] < std::i64::MAX {
+        return dp[i];
     }
 
-    println!("{}", &vec[(N-1) as usize]);
+    // 足場0のコストは0
+    if i == 0 {
+        return 0;
+    }
+
+    let mut res = std::i64::MAX;
+
+    res = min(res, rec(i-1, dp, h) + (h[i] - h[i-1]).abs());
+    if i >= 2 {
+        res = min(res, rec(i-2, dp, h) + (h[i] - h[i-2]).abs());
+    }
+
+    dp[i] = res;
+
+    res
 }
+
+
+// fn main() {
+//     input!{
+//         N: i64,
+//         h: [i64; N],
+//     }
+
+//     let mut vec = vec![1000_000_000_000_000; N as usize];
+
+//     vec[0] = 0;
+//     vec[1] = (&h[1] - &h[0]).abs();
+//     for i in 2..N {
+//         vec[i as usize] = min((&h[i as usize] - &h[(i-1) as usize]).abs() + vec[(i-1) as usize], 
+//                                 vec[i as usize]);
+//         vec[i as usize] = min((&h[i as usize] - &h[(i-2) as usize]).abs() + vec[(i-2) as usize],
+//                                 vec[i as usize]);
+//     }
+
+//     println!("{}", &vec[(N-1) as usize]);
+// }
