d2b6f3607662cf373b11e45f7ed88b47884be696
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-03-27 07:30:17 +0000

    solved

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ https://blog.hamadu.net/2018/08/vscode-rust.html
 # TIPS
 
 ## 配列の最大値、最小値
-`let mut v = vec![5, 6, 8, 4, 2, 7];`
 https://stackoverflow.com/questions/58669865/how-to-get-the-minimum-value-of-a-vector-in-rust
+```
+let ans = dp[n - 1].iter().max().unwrap();
+```
+
+
 
 ## sortしてindexを返す
 https://qiita.com/osanshouo/items/71b0272cd5e156cbf5f2  
--- contest/dp/src/bin/c.rs
+++ contest/dp/src/bin/c.rs
@@ -2,13 +2,33 @@
 use proconio::{input, fastout, marker::Chars};
 #[allow(unused_imports)]
 use std::collections::{HashSet, HashMap, BTreeSet};
+#[allow(unused_imports)]
+use std::cmp::{min, max};
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        N: usize,
+        abc: [[i64; 3]; N],
     }
 
-    println!();
+    let mut dp = vec![vec![0; 3]; N+1];
+
+    for i in 0..3 {
+        dp[1][i] = abc[0][i];
+    }
+
+    for i in 1..N {
+        for j in 0..3 {
+            for k in 0..3 {
+                if j == k {
+                    continue;
+                }
+                dp[i+1][j] = max(dp[i][k] + abc[i][j], dp[i+1][j]);
+            }
+        }
+    }
+
+    println!("{}", max(max(dp[N][0], dp[N][1]), dp[N][2]));
 }
