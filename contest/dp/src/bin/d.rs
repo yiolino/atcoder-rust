47a69b4cceb34b19cb4c26755e914c5a6f812c49
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-03-27 08:40:30 +0000

    sovled

--- contest/dp/src/bin/d.rs
+++ contest/dp/src/bin/d.rs
@@ -7,8 +7,23 @@ use std::collections::{HashSet, HashMap, BTreeSet};
 #[allow(non_snake_case)]
 fn main() {
     input!{
+        N: usize,
+        W: usize,
+        wv: [[usize; 2]; N],
+    }
+
+    let mut dp = vec![vec![0; W+1]; N+1];
+
+    for i in 1..=N {
+        for j in 1..=W {
+            if j as i64 - wv[i-1][0] as i64>= 0 {
+                dp[i][j] = dp[i][j].max(dp[i-1][j - wv[i-1][0]] + wv[i-1][1]);
+            }
+
+            dp[i][j] = dp[i][j].max(dp[i-1][j]);
+        }
         
     }
 
-    println!();
+    println!("{}", dp[N][W]);
 }
