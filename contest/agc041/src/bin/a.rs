34d0a4b1c91c17fb1af9e76ec58e3c3b10a48aef
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-03-30 00:09:47 +0000

    solved

---
+++ contest/agc041/src/bin/a.rs
@@ -0,0 +1,25 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input!{
+        N: i64,
+        A: i64,
+        B: i64,
+    }
+
+    let mut ans = -1;
+    if (B - A) % 2 == 0 {
+        ans = (B - A) / 2;
+    } else {
+        ans = (B - A - 1) / 2 + min(N - B, A - 1) + 1;
+    }
+
+    println!("{}", ans);
+}
