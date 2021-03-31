57e18690a4107e6b3db32f7feca77e6e444d882e
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2021-03-31 23:25:36 +0000

    solved

--- contest/joi2008yo/src/bin/e.rs
+++ contest/joi2008yo/src/bin/e.rs
@@ -9,8 +9,28 @@ use std::cmp::{max, min};
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        R: usize,
+        C: usize,
+        SB: [[usize; C]; R],
     }
 
-    println!();
+    let mut ans = 0;
+    for bit in 0..1 << R {
+        let mut tmp = 0;
+        for c in 0..C {
+            let mut col_sum = 0;
+            for r in 0..R {
+                if bit >> r & 1 > 0 {
+                    col_sum += SB[r][c] ^ 1;  // XORで反転
+                } else {
+                    col_sum += SB[r][c];
+                }
+            }
+            tmp += max(col_sum, R - col_sum);
+        }
+
+        ans = ans.max(tmp);
+    }
+
+    println!("{}", ans);
 }
