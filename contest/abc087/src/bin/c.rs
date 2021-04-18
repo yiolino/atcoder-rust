e2b55c9fa79b07fcb96e24b77373ccf4c5def47b
tetsuro90 <hisayoshi0530@gmail.com>
2021-04-18 03:10:58 +0000

    solved

--- contest/abc087/src/bin/c.rs
+++ contest/abc087/src/bin/c.rs
@@ -1,3 +1,33 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use itertools::Itertools;
+
+#[fastout]
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input!{
+        N: usize,
+        A1: [usize; N],
+        A2: [usize; N],
+    }
+
+    let mut ans = 0;
+    for i in 0..N {
+        let mut tmp = 0;
+        for j in 0..=i {
+            tmp += A1[j];
+        }
+        for j in i..N {
+            tmp += A2[j];
+        }
+
+        ans = ans.max(tmp);
+    }
+
+   println!("{}", ans);
 }
