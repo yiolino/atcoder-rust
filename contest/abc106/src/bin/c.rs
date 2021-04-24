054cc9b61821c27a89b0db7393e11d3daec70461
tetsuro90 <hisayoshi0530@gmail.com>
2021-04-24 05:05:22 +0000

    solved

--- contest/abc106/src/bin/c.rs
+++ contest/abc106/src/bin/c.rs
@@ -4,13 +4,25 @@ use proconio::{input, fastout, marker::Chars};
 use std::collections::{HashSet, HashMap, BTreeSet};
 #[allow(unused_imports)]
 use std::cmp::{max, min};
+#[allow(unused_imports)]
+use itertools::Itertools;
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        S: Chars,
+        K: usize,
     }
 
-    println!();
+    let mut ans = 1;
+    for i in 0..K {
+        let num = S[i] as i64 - 48;
+        if num != 1 {
+            ans = num;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
