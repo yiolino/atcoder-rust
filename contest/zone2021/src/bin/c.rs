1937fe8e7147bae45dece32d4cd12dbcad3d0212
tetsuro90 <hisayoshi0530@gmail.com>
2021-05-03 06:00:02 +0000

    solved

--- contest/zone2021/src/bin/c.rs
+++ contest/zone2021/src/bin/c.rs
@@ -7,12 +7,45 @@ use std::cmp::{max, min};
 #[allow(unused_imports)]
 use itertools::Itertools;
 
-#[fastout]
 #[allow(non_snake_case)]
 fn main() {
-    input!{
-        
+    input! {
+        N: usize,
+        ABCDE: [[usize; 5]; N],
     }
 
-    println!();
+    let mut lower = 0;
+    let mut upper = 1000_000_001;
+
+    'next: while upper - lower > 1 {
+
+        let mid = (lower + upper) / 2;
+        let mut set = HashSet::new();
+
+        for abcde in &ABCDE {
+            let mut x = 0;
+            for (i, &e) in abcde.iter().enumerate() {
+                if e >= mid {
+                    x |= 1 << i;
+                } 
+            }
+
+            set.insert(x);
+        }
+
+        for i in &set {
+            for j in &set {
+                for k in &set {
+                    if i | j | k == (1 << 5) - 1 {
+                        lower = mid;
+                        continue 'next;
+                    }
+                }
+            }
+        }
+
+        upper = mid;
+    }
+
+    println!("{}", lower);
 }
