b39f1c6cb139eefec46e0adbb82c185c42cfb792
tetsuro90 <hisayoshi0530@gmail.com>
2021-05-09 06:59:18 +0000

    solved

--- contest/agc037/src/bin/a.rs
+++ contest/agc037/src/bin/a.rs
@@ -1,12 +1,32 @@
 #[allow(unused_imports)]
 use proconio::{input, fastout, marker::Chars};
 #[allow(unused_imports)]
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use itertools::Itertools;
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input!{
+        S: Chars,
     }
-    println!();
+
+    let mut prev = String::new();
+    let mut buf = String::new();
+
+    let mut ans = 0;
+
+    for s in S {
+        buf.push(s);
+        if prev != buf {
+            prev = buf;
+            buf = String::new();
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
