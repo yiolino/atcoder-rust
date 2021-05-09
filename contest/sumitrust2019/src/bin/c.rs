68f6aad5030a961c2c61e176e69743e286b02ccd
tetsuro90 <hisayoshi0530@gmail.com>
2021-05-09 07:26:17 +0000

    solved

--- contest/sumitrust2019/src/bin/c.rs
+++ contest/sumitrust2019/src/bin/c.rs
@@ -1,14 +1,26 @@
 #[allow(unused_imports)]
 use proconio::{input, fastout, marker::Chars};
 #[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet};
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use itertools::Itertools;
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input!{
+        X: i64,
     }
 
+    let sho = X / 100;
+    let amari = X % 100;
 
-    println!();
+    let mut ans = 0;
+    if amari <= sho * 5 {
+        ans = 1;
+    }
+
+    println!("{}", ans);
 }
