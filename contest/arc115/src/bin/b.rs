687fe0abf36b8ba79782a26820a5ed93b107fb91
tetsuro90 <hisayoshi0530@gmail.com>
2021-05-30 02:32:39 +0000

    solved

--- contest/arc115/src/bin/b.rs
+++ contest/arc115/src/bin/b.rs
@@ -11,8 +11,51 @@ use itertools::Itertools;
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        N: usize,
+        C: [[i64; N]; N],
     }
 
-    println!();
+    // Aの最小値は0で固定する
+    // 条件を満たす列が存在する場合を考える。
+    // C[i][1]列に限定して考えてみると、min(C[i][1]) = 0 + B[1]となる。
+    // よって、B[1]が求まる。続いてA[1]が決められる。
+    // これを繰り返していけば良い。
+
+    let mut A = vec![];
+    let mut B = vec![];
+
+    for j in 0..N {
+        let bj = (0..N).map(|i| C[i][j]).min().unwrap(); 
+        B.push(bj);
+        A.push(C[j][j] - bj);
+    }
+
+    for i in 0..N {
+        for j in 0..N {
+            if C[i][j] != A[i] + B[j] {
+                println!("No");
+                return;
+            }
+        }
+    }
+
+    println!("Yes");
+
+    for i in 0..N {
+        if i == N - 1 {
+            println!("{}", A[i]);
+        } else {
+            print!("{}", A[i]);
+            print!(" ");
+        }
+    }
+
+    for i in 0..N {
+        if i == N - 1 {
+            println!("{}", B[i]);
+        } else {
+            print!("{}", B[i]);
+            print!(" ");
+        }
+    }
 }
