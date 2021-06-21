efc09b01a8f65faf77ce642a86d6ee809c032ea0
tetsuro90 <hisayoshi0530@gmail.com>
2021-06-21 00:23:46 +0000

    solved

--- contest/typical90/src/bin/010.rs
+++ contest/typical90/src/bin/010.rs
@@ -10,8 +10,39 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        cp: [[usize; 2]; n],
+        q: usize,
+        lr: [[usize; 2]; q],
     }
 
-    println!();
+    // 累積和で考える
+    let mut vec_cumsum_c1 = vec![];
+    vec_cumsum_c1.push(0);
+    let mut cumsum_c1 = 0;
+    let mut vec_cumsum_c2 = vec![];
+    vec_cumsum_c2.push(0);
+    let mut cumsum_c2 = 0;
+
+    for i in 0..n {
+        if cp[i][0] == 1 {
+            cumsum_c1 += cp[i][1];
+        } else {
+            cumsum_c2 += cp[i][1];
+        }
+
+        vec_cumsum_c1.push(cumsum_c1);
+        vec_cumsum_c2.push(cumsum_c2);
+    }
+
+
+    for i in 0..q {
+        let l = lr[i][0];
+        let r = lr[i][1];
+
+        let ans_c1 = vec_cumsum_c1[r] - vec_cumsum_c1[l-1];
+        let ans_c2 = vec_cumsum_c2[r] - vec_cumsum_c2[l-1];
+
+        println!("{} {}", ans_c1, ans_c2);
+    }
 }
