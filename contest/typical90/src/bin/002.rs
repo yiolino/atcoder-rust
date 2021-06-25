c8d4b12ddf10766590ec9f46dc4d74392e78c2b4
tetsuro90 <hisayoshi0530@gmail.com>
2021-06-25 14:33:09 +0000

    solved

--- contest/typical90/src/bin/002.rs
+++ contest/typical90/src/bin/002.rs
@@ -10,8 +10,44 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
     }
 
-    println!();
+    for i in 0..1<<n {
+        let mut cnd: String = "".to_string();
+
+        for j in (0..n).rev() {
+            if i & 1 << j == 0 {
+                cnd = cnd + "(";
+            } else {
+                cnd = cnd + ")";
+            }
+        }
+
+        if judge(&cnd) {
+            println!("{}", cnd);
+        }
+    }
+}
+
+
+fn judge(s: &String) -> bool {
+    let mut dep: i64 = 0;
+    let s_chars = s.chars().collect::<Vec<char>>();
+    for i in 0..s.len() {
+        if s_chars[i] == '(' {
+            dep += 1;
+        } else {
+            dep -= 1;
+        }
+
+        if dep < 0 {
+            return false;
+        }
+    }
+    if dep == 0 {
+        return true
+    }
+
+    false
 }
