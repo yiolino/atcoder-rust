ef895ea8ea4c8616a6478a9cd5f78d743671e8cd
tetsuro90 <hisayoshi0530@gmail.com>
2021-06-30 00:06:32 +0000

    solved

--- contest/typical90/src/bin/007.rs
+++ contest/typical90/src/bin/007.rs
@@ -10,8 +10,31 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+        b: [usize; q],
     }
 
-    println!();
+    a.sort();
+
+    for i in 0..q {
+        let bi = b[i];
+
+        // 二分探索
+        let mut lower = 0;
+        let mut upper = n - 1;
+
+        while upper - lower > 1 {
+            let mid = (lower + upper) / 2;
+            if bi >= a[mid] {
+                lower = mid;
+            } else {
+                upper = mid;
+            }
+        }
+
+        let ans = min((a[upper] as i64 - bi as i64).abs(), (a[lower] as i64 - bi as i64).abs());
+        println!("{}", ans);
+    }
 }
