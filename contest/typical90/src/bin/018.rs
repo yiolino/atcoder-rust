3c4f268bdd8325b83093b991f1fe6ac5f095b531
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-06 00:02:04 +0000

    solved

--- contest/typical90/src/bin/018.rs
+++ contest/typical90/src/bin/018.rs
@@ -7,11 +7,33 @@ use std::cmp::{max, min};
 #[allow(unused_imports)]
 use itertools::Itertools;
 
+// pi
+const PI: f64 = std::f64::consts::PI;
+
 #[fastout]
 fn main() {
     input!{
-        
+        t: f64,  // 周回にかかる時間
+        l: f64,  // 観覧車の高さ
+        x: f64,  // 直大像のx座標
+        y: f64,  // 直大像のy座標
+        q: usize,  // クエリ個数
+        e: [f64; q],  // e分後のE8君から見た直大象の俯角
     }
 
-    println!();
+    for ei in e {
+        // 観覧車の座標
+        let _k_x = 0.0;
+        let k_y = - l/2_f64 * (2_f64*PI * ei / t).sin();
+        let k_z = l/2_f64 - (l/2_f64) * (2_f64*PI * ei / t).cos();
+
+        // 直大像と観覧車の水平距離
+        let a = (x*x + (y - k_y)*(y - k_y)).sqrt();
+        let b = k_z;
+        
+        let ans = b.atan2(a) * 180_f64 / PI;  // ラジアンを返すので、度に戻す必要がある。
+
+        println!("{}", ans)
+    }
+
 }
