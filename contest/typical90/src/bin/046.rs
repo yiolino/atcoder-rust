c32c945681fff1fa201e4ddfda332d3a9ce7bad2
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-10 23:43:14 +0000

    solved

--- contest/typical90/src/bin/046.rs
+++ contest/typical90/src/bin/046.rs
@@ -10,8 +10,38 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        mut a: [usize; n],
+        mut b: [usize; n],
+        mut c: [usize; n],
     }
 
-    println!();
+    // 余りベースで考える。
+    let mut cnt_a = vec![0_usize; 46];
+    let mut cnt_b = vec![0_usize; 46];
+    let mut cnt_c = vec![0_usize; 46];
+
+    for i in 0..n {
+        a[i] %= 46;
+        cnt_a[a[i]] += 1;
+
+        b[i] %= 46;
+        cnt_b[b[i]] += 1;
+
+        c[i] %= 46;
+        cnt_c[c[i]] += 1;
+    }
+
+    let mut ans = 0;
+    for i in 0..46 {
+        for j in 0..46 {
+            for k in 0..46 {
+                if (i + j + k) % 46 == 0 {
+                    ans += cnt_a[i] * cnt_b[j] * cnt_c[k];
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
