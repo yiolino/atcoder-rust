02d32dd3de353844bf7982289d3779c889543f50
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-12 23:08:27 +0000

    solved

--- contest/typical90/src/bin/052.rs
+++ contest/typical90/src/bin/052.rs
@@ -10,8 +10,24 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
     }
 
-    println!();
+    let mut vec:Vec<usize> = vec![];
+
+    for _ in 0..n {
+        input!{
+            a: [usize; 6],
+        }
+
+        vec.push(a.iter().sum());
+    }
+
+    let mut ans = 1;
+    for v in vec {
+        ans *= v;
+        ans %= 1_000_000_000 + 7;
+    }
+
+    println!("{}", ans);
 }
