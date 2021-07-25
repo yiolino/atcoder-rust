21756e48f83667c350922e132325384b448099dd
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-25 21:49:24 +0000

    soved

--- contest/typical90/src/bin/076.rs
+++ contest/typical90/src/bin/076.rs
@@ -10,8 +10,44 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n]
     }
 
-    println!();
+    let mut vec = vec![];
+    let mut cnt = 0;
+    vec.push(cnt);
+
+    for i in 0..2*n {
+        cnt += a[i % n];
+        vec.push(cnt);
+    }
+
+    if vec[n] % 10 != 0 {
+        println!("No");
+        return;
+    }
+
+
+    for i in 0..n {
+        let goal = vec[i] + vec[n]/10;
+        let mut low = 0;
+        let mut high = 2*n;
+
+        while high - low > 1 {
+            let mid = (low + high) / 2;
+            if vec[mid]  <= goal {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        if vec[low] == goal {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
