bcab49359a8c90ccf75ccc7ba64bfb42d8d8eb69
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-25 23:06:40 +0000

    solved

--- README.md
+++ README.md
@@ -108,13 +108,62 @@ fn main() {
 }
 ```
 
+
 ## combination
 ```
 let vec:Vec<Vec<i64>> = (0..N).combinations(2).collect();
 ```
 
+
 ## 二分探索 upper_bound, lower_bound
 https://github.com/hatoo/competitive-rust-snippets/blob/master/src/binary_search.rs
+```
+use std::cmp::Ordering;
+/// Equivalent to std::lowerbound and std::upperbound in c++
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+}
+```
+
 
 ## 最小公倍数、最大公約数
 ```
--- contest/typical90/src/bin/076.rs
+++ contest/typical90/src/bin/076.rs
@@ -7,6 +7,9 @@ use std::cmp::{max, min};
 #[allow(unused_imports)]
 use itertools::Itertools;
 
+
+
+
 #[fastout]
 fn main() {
     input!{
@@ -31,19 +34,10 @@ fn main() {
 
     for i in 0..n {
         let goal = vec[i] + vec[n]/10;
-        let mut low = 0;
-        let mut high = 2*n;
 
-        while high - low > 1 {
-            let mid = (low + high) / 2;
-            if vec[mid]  <= goal {
-                low = mid;
-            } else {
-                high = mid;
-            }
-        }
+        let cnd = vec.lower_bound(&goal);
 
-        if vec[low] == goal {
+        if vec[cnd] == goal {
             println!("Yes");
             return;
         }
@@ -51,3 +45,81 @@ fn main() {
 
     println!("No");
 }
+
+
+
+use std::cmp::Ordering;
+/// Equivalent to std::lowerbound and std::upperbound in c++
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+}
+
+
+
+
+
+// lower_boundを使わないバージョン
+// #[fastout]
+// fn main() {
+//     input!{
+//         n: usize,
+//         a: [usize; n]
+//     }
+
+//     let mut vec = vec![];
+//     let mut cnt = 0;
+//     vec.push(cnt);
+
+//     for i in 0..2*n {
+//         cnt += a[i % n];
+//         vec.push(cnt);
+//     }
+
+//     if vec[n] % 10 != 0 {
+//         println!("No");
+//         return;
+//     }
+
+
+//     for i in 0..n {
+//         let goal = vec[i] + vec[n]/10;
+//         let mut low = 0;
+//         let mut high = 2*n;
+
+//         while high - low > 1 {
+//             let mid = (low + high) / 2;
+//             if vec[mid]  <= goal {
+//                 low = mid;
+//             } else {
+//                 high = mid;
+//             }
+//         }
+
+//         if vec[low] == goal {
+//             println!("Yes");
+//             return;
+//         }
+//     }
+
+//     println!("No");
+// }
