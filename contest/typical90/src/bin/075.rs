6e05727125b1002467ed197276927b390fbe541d
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-25 02:24:26 +0000

    solved:

--- contest/typical90/src/bin/075.rs
+++ contest/typical90/src/bin/075.rs
@@ -10,8 +10,34 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        mut n: usize,
     }
 
-    println!();
+    let mut prime_vec = vec![];
+    let mut cnd = 2;
+
+    while cnd * cnd <= n {
+        if n % cnd == 0 {
+            prime_vec.push(cnd);
+            n /= cnd;
+
+        } else {
+            cnd += 1;
+        }
+    }
+
+    if n != 1 {
+        prime_vec.push(n);
+    }
+
+    let mut ans = 0;
+    let mut cnt = 1;
+
+    while cnt < prime_vec.len() {
+        cnt *= 2;
+        ans += 1;
+    }
+
+    println!("{}", ans);
+
 }
