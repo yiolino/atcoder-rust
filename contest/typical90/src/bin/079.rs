39424d286685f736d26bdae6c2fc226d6f9fc830
tetsuro90 <hisayoshi0530@gmail.com>
2021-07-26 13:49:14 +0000

    solved 079

--- contest/typical90/src/bin/079.rs
+++ contest/typical90/src/bin/079.rs
@@ -10,8 +10,29 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        h: usize,
+        w: usize,
+        mut a: [[i64; w]; h],
+        b: [[i64; w]; h],
     }
 
-    println!();
+    let mut ans = 0;
+
+    for i in 0..h-1 {
+        for j in 0..w-1 {
+            let d = b[i][j] - a[i][j];
+            a[i][j] += d;
+            a[i][j+1] += d;
+            a[i+1][j] += d;
+            a[i+1][j+1] += d;
+            ans += d.abs();
+        }
+    }
+
+    if a == b {
+        println!("Yes");
+        println!("{}", ans)
+    } else {
+        println!("No");
+    }
 }
