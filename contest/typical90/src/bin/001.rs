599cd3ddcc5995f70281c57be66cf14f656b1a7c
tetsuro90 <hisayoshi0530@gmail.com>
2021-08-14 00:26:54 +0000

    solved

--- contest/typical90/src/bin/001.rs
+++ contest/typical90/src/bin/001.rs
@@ -10,8 +10,47 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
+        n: usize,
+        l: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    // 配列aを隣との差の配列に変える。
+    let mut b = vec![];
+    b.push(a[0]);
+    for i in 0..n-1 {
+        b.push(a[i+1] - a[i]);
+    }
+    b.push(l as usize - a[n-1] as usize);
+
+    // 二分探索
+    let mut lower = 0;
+    let mut higher = 1_000_000_000 + 1;
+
+    while higher - lower > 1 {
+        let mid = (higher + lower) / 2;
+
+        let mut cnt = 0;
+        let mut tmp = 0;
+
+        for i in 0..=n {
+            tmp += b[i];
+
+            if tmp >= mid {
+                cnt += 1;
+                tmp = 0;
+            }
+        }
+
+        if cnt >= k + 1 {
+            lower = mid;
+        } else {
+            higher = mid;
+        }
         
     }
 
-    println!();
+
+    println!("{}", lower);
 }
