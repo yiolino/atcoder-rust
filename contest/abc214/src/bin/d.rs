3866bac3887bc11f94b114abfa2ab76d4d9963bc
tetsuro90 <hisayoshi0530@gmail.com>
2021-08-15 23:08:20 +0000

    solved

--- contest/abc214/src/bin/d.rs
+++ contest/abc214/src/bin/d.rs
@@ -7,7 +7,96 @@ use std::cmp::{max, min, Reverse};
 #[allow(unused_imports)]
 use itertools::Itertools;
 
+
+
 #[fastout]
 fn main() {
+    input!{
+        n: usize,
+        mut uvw: [(usize, usize, usize); n - 1],
+    }
 
+    // 重みで並び替え
+    uvw.sort_by_key(|s| s.2);
+
+    // 頂点数nのunion-find構造体
+    let mut uf = UnionFind::new(n);
+
+    // 辺の重みが小さい方から計算していく
+    let mut cnt = 0;
+    for (u, v, w) in &mut uvw {
+        *u -= 1;
+        *v -= 1;
+        cnt += *w * uf.return_size(*u) * uf.return_size(*v);
+        uf.unite(*u, *v);
+    }
+
+    println!("{}", cnt);
+}
+
+
+
+//---------- begin union_find by @nakamurus ----------
+struct UnionFind {
+    par: Vec<usize>,  // 各頂点の親頂点の番号を表す。
+    siz: Vec<usize>,  // 各頂点の属する根付き木の頂点数を表す
+}
+
+#[allow(unused)]
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        // 初期値は自身が自身の根出る（各木の頂点数は1）
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+
+    // 根を返す。経路圧縮による効率化の実装
+    fn find_root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;  // xが根の場合はxを返す。
+        }
+        self.par[x] = self.find_root(self.par[x]);
+        self.par[x]
+    }
+
+
+    // xとyが同じ木に属するか判定
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find_root(x) == self.find_root(y)
+    }
+
+
+    // xの木とyの木を併合する. 関数の戻り値はboolであることに注意
+    fn unite(&mut self, mut x: usize, mut y: usize) -> bool {
+        // x, y それぞれ根まで移動する
+        x = self.find_root(x);
+        y = self.find_root(y);
+
+        // 既に同じ木だった場合は何もしない
+        if x == y  {
+            return false;
+        }
+
+        // uion by size（y側の木の頂点数が小さくなるようにする）
+        if self.siz[x] < self.siz[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+
+        // yをxの子とする
+        self.par[y] = x;
+        self.siz[x] += self.siz[y];
+        
+        true
+    }
+
+
+    // 
+    fn return_size(&mut self, x: usize) -> usize {
+        let root = self.find_root(x);
+        self.siz[root]
+    }
+    
 }
