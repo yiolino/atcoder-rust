bd3953a8b2423330d326c239be1d4d060d755742
tetsuro90 <hisayoshi0530@gmail.com>
2021-09-19 15:41:38 +0900

    solved

--- contest/abc219/src/bin/d.rs
+++ contest/abc219/src/bin/d.rs
@@ -12,8 +12,39 @@ use petgraph::unionfind::UnionFind;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        x: usize, y: usize,
+        ab: [[usize; 2]; n],
     }
 
-    println!();
+    // 動的計画法で解く
+    // dp[i][j][k]: 0~i番目までの弁当を買うか買わないかが決定しており、j個のたこ焼きとk個のたい焼きが手に入っている状態での最小の弁当の個数
+
+    let mut dp = vec![vec![vec![1000; y+1]; x+1]; n+1];
+
+    dp[0][0][0] = 0;
+
+    for i in 1..=n {
+        let ai = ab[i-1][0];
+        let bi = ab[i-1][1];
+
+        for j in 0..=x {
+            for k in 0..=y {
+                // i-1, j, k状態からi番目の弁当を買うか買わないかを考える。
+                
+                // 買う場合、i, j+ai, k+biになる。
+                dp[i][min(j+ai, x)][min(k+bi, y)] = min(dp[i-1][j][k] + 1, dp[i][min(j+ai, x)][min(k+bi, y)]);
+                // 買わない場合、i, j, kになる。
+                dp[i][j][k] = min(dp[i][j][k], dp[i-1][j][k]);
+            }
+        }
+    }
+
+    let ans = if dp[n][x][y] == 1000 {
+        -1
+    } else {
+        dp[n][x][y] as i64
+    };
+
+    println!("{}", ans);
 }
