fa3f40a582c6d0bf531e430303b001a078debc89
tetsuro90 <hisayoshi0530@gmail.com>
2021-10-13 23:25:57 +0900

    sovled

--- contest/typical90/src/bin/008.rs
+++ contest/typical90/src/bin/008.rs
@@ -10,8 +10,30 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        mut s: Chars,
     }
 
-    println!();
+    // 状態DP
+    // 8行, n列のマトリクス
+    let mut dp = vec![vec![0; n+1]; 8];
+    for i in 0..=n {
+        dp[0][i] = 1;
+    }
+
+    let atcoder = vec!['-', 'a', 't', 'c', 'o', 'd', 'e', 'r'];
+    s.insert(0, '-');
+
+    for i in 1..=7 {
+        for j in 1..=n {
+            if s[j] == atcoder[i] {
+                dp[i][j] += dp[i-1][j-1];
+                dp[i][j] %= 1_000_000_000 + 7;
+            }
+            dp[i][j] += dp[i][j-1];
+            dp[i][j] %= 1_000_000_000 + 7;
+        }
+    }
+
+    println!("{}", dp[7][n]);
 }
