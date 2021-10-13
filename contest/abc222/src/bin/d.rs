4b59e3160bb901c6377f7d1b61f43f401c31cc54
tetsuro90 <hisayoshi0530@gmail.com>
2021-10-13 09:11:51 +0900

    solved

--- contest/abc222/src/bin/d.rs
+++ contest/abc222/src/bin/d.rs
@@ -19,7 +19,35 @@ fn main() {
         mut b: [usize; n],
     }
 
-
+    // 0番目を作っておく
+    a.insert(0, 0);
+    b.insert(0, 0);
     
-    println!();
+    let m = *max(a.iter().max().unwrap(), b.iter().max().unwrap());
+
+    let mut dp = vec![vec![0; m+1]; n+1];
+    let mut r = vec![vec![0; m+1]; n+1];  // 累積和用のテーブル
+
+    dp[0][0] = 1;
+    for i in 0..=m {
+        r[0][i] = 1;
+    }
+
+    for i in 1..=n {
+        for j in 0..=m {
+            if a[i] <= j && j <= b[i] {
+                    if j == 0 {
+                        dp[i][j] = 1;
+                        r[i][j] = 1;
+                    }
+                    dp[i][j] = r[i-1][j] % 998244353;
+                }
+            
+            if j != 0 {
+                r[i][j] = (r[i][j-1] + dp[i][j]) % 998244353;
+            }
+        }
+    }
+
+    println!("{}", r[n][m]);
 }
