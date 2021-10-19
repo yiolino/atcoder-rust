b11c889a38961dc243e10118f9b4718a08be271b
tetsuro90 <hisayoshi0530@gmail.com>
2021-10-19 23:00:08 +0900

    solved dp

--- contest/typical90/src/bin/056.rs
+++ contest/typical90/src/bin/056.rs
@@ -10,8 +10,60 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        s: usize,
+        ab: [(usize, usize); n],
     }
 
-    println!();
+    let mut dp = vec![vec![false; s+1]; n+1];
+    dp[n][s] = true;
+
+    for i in (1..=n).rev() {
+        for j in (0..=s).rev() {
+            if dp[i][j] {
+                let a = j.checked_sub(ab[i.wrapping_sub(1)].0);
+                let b = j.checked_sub(ab[i.wrapping_sub(1)].1);
+
+                if let Some(v_a) = a {
+                    dp[i.wrapping_sub(1)][v_a] = true;
+                }
+                if let Some(v_b) = b {
+                    dp[i.wrapping_sub(1)][v_b] = true;
+                }
+
+            }
+        }
+    }
+
+    if dp[0][0] {
+        let mut pos = 0;
+
+        for i in 1..=n {
+            let a = ab[i.wrapping_sub(1)].0;
+            let b = ab[i.wrapping_sub(1)].1;
+
+            let flg = dp[i].get(pos + a);
+            if let Some(f) = flg {
+                if *f {
+                    pos += a;
+                    print!("A");
+                    continue;
+                }
+            }
+
+            let flg = dp[i].get(pos + b);
+            if let Some(f) = flg {
+                if *f {
+                    pos += b;
+                    print!("B");
+                    continue;
+                }
+            }
+        }
+        println!();
+    
+    } else {
+        println!("Impossible");
+    }
+
 }
