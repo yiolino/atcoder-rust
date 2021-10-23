f0b838c363605cc08428bf7da4532acedc1a5b20
tetsuro90 <hisayoshi0530@gmail.com>
2021-10-23 15:27:47 +0900

    solved

--- contest/typical90/src/bin/011.rs
+++ contest/typical90/src/bin/011.rs
@@ -10,8 +10,35 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        mut dcs:[(usize,usize,usize); n],
     }
 
-    println!();
+    // 締め切りの順にsort
+    dcs.sort_by_key(|p| p.0);
+
+    // 動的計画法
+    let max_d = dcs[n-1].0;
+    let mut dp = vec![vec![0; max_d+1]; n+1];
+
+    for i in 1..=n {
+        let d = dcs[i.wrapping_sub(1)].0;
+        let c = dcs[i.wrapping_sub(1)].1;
+        let s = dcs[i.wrapping_sub(1)].2;
+
+        for j in 0..=max_d {
+            if j < c || j > d {
+                dp[i][j] = dp[i.wrapping_sub(1)][j];
+            } else {
+                dp[i][j] = max(dp[i.wrapping_sub(1)][j.wrapping_sub(c)] + s, dp[i.wrapping_sub(1)][j]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..=max_d {
+        ans = ans.max(dp[n][i]);
+    }
+
+    println!("{}", ans);
 }
