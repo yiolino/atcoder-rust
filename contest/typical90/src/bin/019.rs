6be6a5bab129b6c519653bb44f3d048a5d5f78ba
tetsuro90 <hisayoshi0530@gmail.com>
2021-10-27 22:57:20 +0900

    solved

--- contest/typical90/src/bin/019.rs
+++ contest/typical90/src/bin/019.rs
@@ -10,8 +10,28 @@ use itertools::Itertools;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        a: [i32; 2*n],
     }
 
-    println!();
+    let mut dp = vec![vec![0; 2*n]; 2*n];
+    
+    // まず隣り合う同士を引き抜く時を全て埋めてしまう。
+    for i in 0..(2*n-1) {
+        dp[i][i+1] = (a[i] - a[i+1]).abs();
+    }
+
+    for i in (3..2*n).step_by(2) {
+        for j in 0..(2*n - i) {
+            let mut val = (a[j] - a[j+i]).abs() + dp[j+1][(j+i).wrapping_sub(1)];
+            for k in (j+1..(j+i)).step_by(2) {
+                val = val.min(dp[j][k] + dp[k+1][j+i]);
+            }
+            dp[j][j+i] = val;
+        }
+    }
+    
+    let ans = dp[0][2*n -1];
+    println!("{}", ans);
 }
+
