79e7f17357dcea44f735e8ceb66f0d71fdf3428c
tetsuro90 <hisayoshi0530@gmail.com>
2021-11-12 08:55:29 +0900

    solved EDPC d

--- contest/dp/src/bin/d.rs
+++ contest/dp/src/bin/d.rs
@@ -14,8 +14,27 @@ use superslice::Ext;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        w: usize,
+        mut wv: [(usize, usize); n],
     }
 
-    println!();
+    let mut dp = vec![vec![0; w+1]; n+1];
+    wv.insert(0, (0, 0));
+
+    for i in 1..=n {
+        for j in 0..=w {
+            if j < wv[i].0 {
+                dp[i][j] = dp[i.wrapping_sub(1)][j];
+            } else {
+                let tuika = dp[i.wrapping_sub(1)][j.wrapping_sub(wv[i].0)] + wv[i].1;
+                let sonomama = dp[i.wrapping_sub(1)][j];
+                dp[i][j] = max(tuika, sonomama);
+            }
+        }
+    }
+
+    let ans = dp[n].iter().max().unwrap();
+
+    println!("{}", ans);
 }
