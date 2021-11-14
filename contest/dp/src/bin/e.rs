12f025ea787a136b81e15b300749f46407c7a8ee
tetsuro90 <hisayoshi0530@gmail.com>
2021-11-15 08:47:04 +0900

    solved EDDP e

--- contest/dp/src/bin/e.rs
+++ contest/dp/src/bin/e.rs
@@ -14,8 +14,40 @@ use superslice::Ext;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        w: usize,
+        mut wv: [(usize, usize); n],
     }
 
-    println!();
+    // n, vに関するdpマトリックスを作成する。
+    let mut dp = vec![vec![w+1; 1e5 as usize + 1]; n+1];
+    for i in 0..=n {
+        dp[i][0] = 0;
+    }
+
+    wv.insert(0, (0, 0));
+
+
+    for i in 1..=n {
+        for j in 0..=(1e5 as usize) {
+            if j >= wv[i].1 {
+                let w1 = dp[i.wrapping_sub(1)][j];
+                let w2 = dp[i.wrapping_sub(1)][j.wrapping_sub(wv[i].1)];
+                dp[i][j] = min(w1, w2 + wv[i].0);
+
+            } else {
+                dp[i][j] = dp[i.wrapping_sub(1)][j];
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for j in (0..=(1e5 as usize)).rev() {
+        if dp[n][j] <= w {
+            ans = j;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
