26243388c2ff88a16f17459ed35380c7417e9b25
tetsuro90 <hisayoshi0530@gmail.com>
2021-11-16 13:16:43 +0900

    solved

--- contest/dp/src/bin/f.rs
+++ contest/dp/src/bin/f.rs
@@ -4,6 +4,7 @@ use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
 use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
 #[allow(unused_imports)]
 use std::cmp::{max, min, Reverse};
+use std::vec;
 #[allow(unused_imports)]
 use itertools::Itertools;
 #[allow(unused_imports)]
@@ -14,8 +15,49 @@ use superslice::Ext;
 #[fastout]
 fn main() {
     input!{
-        
+        mut s: Chars,
+        mut t: Chars,
     }
 
-    println!();
+    let slen = s.len();
+    let tlen = t.len();
+    s.insert(0, '0');
+    t.insert(0, '1');
+
+    // dp[i][j] := 「sをi文字目、tをj文字目までみた時の最長共通部分裂の長さ」 とする
+    let mut dp = vec![vec![0; t.len()]; s.len()];
+
+    for i in 1..s.len() {
+        for j in 1..t.len() {
+            if s[i] == t[j] {
+                dp[i][j] = dp[i.wrapping_sub(1)][j.wrapping_sub(1)] + 1;
+            } else {
+                dp[i][j] = max(dp[i.wrapping_sub(1)][j], dp[i][j.wrapping_sub(1)]);
+            } 
+        }
+    }
+
+    let mut seq = vec![];
+    let mut length = dp[s.len() - 1][t.len() - 1];
+    let mut i = slen;
+    let mut j = tlen;
+
+    while length > 0 {
+        if s[i] == t[j] {
+            seq.push(s[i]);
+            length -= 1;
+            i -= 1;
+            j -= 1;
+
+        } else if dp[i][j] == dp[i.wrapping_sub(1)][j] {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+
+    seq.reverse();
+    let ans = seq.into_iter().collect::<String>();
+
+    println!("{}", ans);
 }
