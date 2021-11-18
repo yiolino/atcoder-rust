1be50f76f74d693d78a54e2fbcd82ab4001cc470
tetsuro90 <hisayoshi0530@gmail.com>
2021-11-19 08:28:13 +0900

    solved DEPC h: :

--- contest/dp/src/bin/h.rs
+++ contest/dp/src/bin/h.rs
@@ -14,8 +14,35 @@ use superslice::Ext;
 #[fastout]
 fn main() {
     input!{
-        
+        h: usize,
+        w: usize,
+        hs: [Chars; h],
     }
 
-    println!();
+    let mut dp = vec![vec![0; w]; h];
+
+    for i in 0..h {
+        if hs[i][0] == '#' {
+            break;
+        }
+        dp[i][0] = 1;
+    }
+    for i in 0..w {
+        if hs[0][i] == '#' {
+            break;
+        }
+        dp[0][i] = 1;
+    }
+
+    
+    for i in 1..h {
+        for j in 1..w {
+            if hs[i][j] != '#' {
+                dp[i][j] = dp[i.wrapping_sub(1)][j] + dp[i][j.wrapping_sub(1)];
+                dp[i][j] %= 1_000_000_000 + 7;
+            }
+        }
+    }
+
+    println!("{}", dp[h.wrapping_sub(1)][w.wrapping_sub(1)]);
 }
