aa8a9545903b07fa2b69f2fc72d3b02f52bc239d
tetsuro90 <hisayoshi0530@gmail.com>
2021-11-21 10:58:48 +0900

    solved EDPC i

--- contest/dp/src/bin/i.rs
+++ contest/dp/src/bin/i.rs
@@ -14,8 +14,32 @@ use superslice::Ext;
 #[fastout]
 fn main() {
     input!{
-        
+        n: usize,
+        mut p: [f64; n],
     }
 
-    println!();
+    p.insert(0, 0.0);
+
+    // dp[i][j] := i枚のコインを投げたときに、j枚の表が出ている確率 と定義する。
+    let mut dp = vec![vec![0.0; n+1]; n+1];
+    dp[1][1] = p[1];
+    dp[1][0] = 1.0 - p[1];
+    for i in 2..=n {
+        dp[i][0] = dp[i.wrapping_sub(1)][0] * (1.0-p[i]);
+    }
+
+
+    for i in 2..=n {
+        for j in 1..=i {
+            let pi = p[i];
+            dp[i][j] = dp[i.wrapping_sub(1)][j.wrapping_sub(1)] * pi + dp[i.wrapping_sub(1)][j] * (1.0-pi);
+        }
+    }
+
+    let mut ans = 0.0;
+    for i in (n/2+1)..=n {
+        ans += dp[n][i];
+    }
+
+    println!("{}", ans);
 }
