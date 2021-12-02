5de47dfaaa0009141182d00e745440591ffa49b6
tetsuro90 <hisayoshi0530@gmail.com>
2021-12-03 08:43:14 +0900

    solved EDPC l

--- contest/dp/src/bin/l.rs
+++ contest/dp/src/bin/l.rs
@@ -11,11 +11,33 @@ use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
+
 fn main() {
     input!{
-        
+        n: usize,
+        a: [i64; n],
     }
 
-    println!();
+    // dp[l][r] := 区間[l, r)が残っているとき、双方がその後最善を尽くした時のスコア
+    // 先行、後攻で分けて考えたいところだが、%2の余りでどちらが引くか自動的にわかる
+    let mut dp = vec![vec![0; n+1]; n+1];
+
+    for len in 1..=n {
+        for l in 0..=(n-len) {
+            let r = l + len;
+            if (n-len) % 2 == 0 {
+                let v1 = dp[l+1][r] + a[l];
+                let v2 = dp[l][r.wrapping_sub(1)] + a[r.wrapping_sub(1)];
+                dp[l][r] = max(v1, v2);
+
+            } else {
+                let v1 = dp[l+1][r] - a[l];
+                let v2 = dp[l][r.wrapping_sub(1)] - a[r.wrapping_sub(1)];
+                dp[l][r] = min(v1, v2);
+            }
+        }
+    }
+
+
+    println!("{}", dp[0][n]);
 }
