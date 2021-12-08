093635c96e944daba789ea0fb303d45c5099c91b
tetsuro90 <hisayoshi0530@gmail.com>
2021-12-09 08:26:37 +0900

    solved

--- contest/dp/src/bin/n.rs
+++ contest/dp/src/bin/n.rs
@@ -11,11 +11,38 @@ use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
+
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n],
     }
 
-    println!();
+    // dp[l][r] = [l, r) l以上r未満のスライムを1体にするときのコストの最小値
+    // 右は閉区間であることに注意
+    let mut dp = vec![vec![0; n+1]; n+1];
+
+
+    // 累積和を持っておく
+    let mut cum = vec![vec![0; n+1]; n+1];
+    for i in 0..n {
+        cum[i][i+1] = a[i];
+        for j in i+2..=n {
+            cum[i][j] = cum[i][j-1] + a[j-1];
+        }
+    }
+    
+    for len in 2..=n {
+        for l in 0..=n-len {
+            let r = l+len;
+            dp[l][r] = std::usize::MAX;
+
+            for m in l+1..r {
+                dp[l][r] = dp[l][r].min(cum[l][r] + dp[l][m] + dp[m][r]);
+            }
+
+        }
+    }
+
+    println!("{}", dp[0][n]);
 }
