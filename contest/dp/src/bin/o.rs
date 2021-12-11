9518d13f39fa51c8d9ae19ade0816749b2c9f4b6
tetsuro90 <hisayoshi0530@gmail.com>
2021-12-11 14:44:02 +0900

    solved EDPC o

--- contest/dp/src/bin/o.rs
+++ contest/dp/src/bin/o.rs
@@ -4,6 +4,7 @@ use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
 use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
 #[allow(unused_imports)]
 use std::cmp::{max, min, Reverse};
+use std::vec;
 #[allow(unused_imports)]
 use itertools::Itertools;
 #[allow(unused_imports)]
@@ -11,11 +12,30 @@ use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
+
 fn main() {
     input!{
-        
+        n: usize,
+        a: [[usize; n];n],
     }
 
-    println!();
+    // dpをどう持つか？
+    // dp[S] := 左からi人めの男性と集合Sに属する女性とでペアを作るときの場合の数。
+    // Sはbitとして集合を表す。
+    let mut dp = vec![0; 1 << n];
+    dp[0] = 1;
+    let mut bitcount = vec![0; 1 << n];
+
+    for s in 1..(1 << n) {
+        bitcount[s] = bitcount[s/2] + s%2;
+        for l in 0..n {
+            // l ... l番目の女性
+            if (s >> l & 1) > 0 && a[bitcount[s]-1][l] == 1 {
+                dp[s] += dp[s - (1 << l)];
+                dp[s] %= 1_000_000_000 + 7;
+            }
+        }
+    }
+
+    println!("{}", dp[(1<<n) - 1]);
 }
