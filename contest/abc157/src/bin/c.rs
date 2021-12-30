ebbf6d0d408f6a38fa58f6002b969c68827a112b
tetsuro90 <hisayoshi0530@gmail.com>
2021-12-30 22:50:26 +0900

    solved

--- contest/abc157/src/bin/c.rs
+++ contest/abc157/src/bin/c.rs
@@ -17,44 +17,37 @@ fn main() {
         m: usize,
     }
 
-    let mut vec = vec!['a'; n];
+    let mut s = vec![];
+    let mut c = vec![];
 
     for _ in 0..m {
-        input! {s: usize, c: char};
+        input! {mut ss: usize, cc: char,};
 
-        if vec[s-1] == 'a' {
-            vec[s-1] = c;
-        } else if vec[s-1] != c {
-            println!("-1");
-            return;
-        }
+        ss -= 1;
+        s.push(ss);
+        c.push(cc);
     }
 
-    // 2桁以上かどうかで場合分け
-    if n > 1 {
-        if vec[0] == '0' {
-            println!("-1");
-            return;
-        } else {
-            for i in 0..n {
-                if i == 0 && vec[i] == 'a' {
-                    vec[0] = '1';
-                } else {
-                    if vec[i] == 'a' {
-                        vec[i] = '0';
-                    }
-                }
+    // 0 ~ 1000までチェックする
+    for ans in 0..1000 {
+        let tmp = ans.to_string().chars().into_iter().collect::<Vec<char>>();
+        if tmp.len() != n {
+            continue;
+        }
+        let mut is_ok = true;
+
+        for j in 0..m {
+            let idx = s[j];
+            if tmp[idx] != c[j] {
+                is_ok = false;
             }
+        }
 
-            let ans = vec.into_iter().collect::<String>();
+        if is_ok {
             println!("{}", ans);
+            return;
         }
-
-    } else {
-        if vec[0] == 'a' {
-            vec[0] = '0';
-        }
-
-        println!("{}", vec[0]);
     }
+
+    println!("-1");
 }
