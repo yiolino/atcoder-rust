9f30e705ca6605d7e4e50b8eed96bb68cf632b55
tetsuro90 <hisayoshi0530@gmail.com>
2022-01-04 10:43:43 +0900

    solved

---
+++ contest/code-festival-2014-morning-easy/src/bin/a.rs
@@ -0,0 +1,29 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input!{
+        n: usize,
+        a: [f64; n],
+    }
+
+    let mut ans = 0.0;
+    for i in 0..n-1 {
+        ans += a[i+1] - a[i];
+    }
+
+    ans /= (n-1) as f64;
+
+
+    println!("{}", ans);
+}
