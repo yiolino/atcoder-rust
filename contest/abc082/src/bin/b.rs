0ef526c52d3510cb77ab509953dd063932f24e9a
tetsuro90 <hisayoshi0530@gmail.com>
2022-01-08 14:10:58 +0900

    solved abc082 a

---
+++ contest/abc082/src/bin/b.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input!{
+        mut s: Chars,
+        mut t: Chars,
+    }
+
+    s.sort();
+    t.sort();
+    t.reverse();
+
+    let ans = if s < t {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
+}
