e6c196bf9100979d77b4f0aeb491e7ed9b59c3bf
tetsuro90 <hisayoshi0530@gmail.com>
2022-01-09 09:11:40 +0900

    solved abc060 b

---
+++ contest/abc060/src/bin/b.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input!{
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+
+    let mut ans = "NO";
+
+    for i in 1..1000_000 {
+        if a * i % b == c {
+            ans = "YES";
+            break;
+        }
+    }
+
+    println!("{}", ans);
+}
