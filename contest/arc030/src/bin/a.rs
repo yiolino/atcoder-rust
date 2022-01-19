c03bc5ed1b22134320c88763af8de333883bcc00
tetsuro90 <hisayoshi0530@gmail.com>
2022-01-19 22:23:39 +0900

    solved

---
+++ contest/arc030/src/bin/a.rs
@@ -0,0 +1,27 @@
+#[allow(unused_imports)]
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
+#[allow(unused_imports)]
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input!{
+        n: usize,
+        k: usize,
+    }
+
+    let div = n / 2;
+
+    if div >= k {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
