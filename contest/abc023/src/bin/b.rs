0331d0aa6762b04a40327e4f87db63f36826b0aa
tetsuro90 <hisayoshi0530@gmail.com>
2022-01-27 21:58:20 +0900

    solved

--- contest/abc023/src/bin/b.rs
+++ contest/abc023/src/bin/b.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use proconio::{input, fastout, marker::Chars};
+use std::collections::VecDeque;
 #[allow(unused_imports)]
 use std::collections::{HashSet, HashMap, BTreeSet};
 #[allow(unused_imports)]
@@ -7,12 +8,33 @@ use std::cmp::{max, min};
 #[allow(unused_imports)]
 use itertools::Itertools;
 
-#[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        _n: usize,
+        s: String,
+    }
+    if s == "b".to_string() {
+        println!("0");
+        return;
     }
 
-    println!();
+    let mut deque = VecDeque::new();
+    deque.push_back('b');
+
+    for i in 1..1000 {
+        match i % 3 {
+            1 => {deque.push_front('a'); deque.push_back('c')},
+            2 => {deque.push_front('c'); deque.push_back('a')},
+            _ => {deque.push_front('b'); deque.push_back('b')},
+        }
+
+        let tmp = deque.iter().collect::<String>();
+        if tmp == s {
+            println!("{}", i);
+            return;
+        }
+    }
+
+    println!("-1");
 }
