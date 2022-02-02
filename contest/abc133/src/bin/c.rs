b6400063fc8c65063d9874187c7dc6bc268ecc7c
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-02 12:57:28 +0900

    solved

--- contest/abc133/src/bin/c.rs
+++ contest/abc133/src/bin/c.rs
@@ -1,14 +1,33 @@
 #[allow(unused_imports)]
-use proconio::{input, fastout, marker::Chars};
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
 #[allow(unused_imports)]
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
 
-#[fastout]
-#[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        l: usize,
+        r: usize,
     }
 
-    println!();
+    let mut ans = std::usize::MAX;
+
+    for i in l..l+2019 {
+        for j in i+1..l+2019 {
+            if i > r || j > r {
+                continue;
+            }
+
+            ans = ans.min(i * j % 2019);
+        }
+    }
+
+    println!("{}", ans);
 }
