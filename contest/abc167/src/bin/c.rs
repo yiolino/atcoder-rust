b9d05b2f340d20938b8a98986884239ef3808600
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-04 23:57:12 +0900

    solved

--- contest/abc167/src/bin/c.rs
+++ contest/abc167/src/bin/c.rs
@@ -19,13 +19,54 @@ fn main() {
         c_a: [[usize; m + 1]; n],
     }
 
-    // dfsで解いてみる
+    // dfsの全探索で解いてみる
+    let dfs = DFS::new(n, m, x, c_a);
 
-    println!();
+    let res = dfs.dfs(0, vec![0; m + 1]);
+
+    if res < std::usize::MAX {
+        println!("{}", res);    
+    } else {
+        println!("-1");
+    }
 }
 
 
 struct DFS {
-    total_price: usize,
-    
+    n: usize,
+    m: usize,
+    x: usize,
+    c_a: Vec<Vec<usize>>,
+}
+
+impl DFS {
+    fn new(n: usize, m: usize, x: usize, c_a: Vec<Vec<usize>>) -> Self {
+        DFS {
+            n: n,
+            m: m,
+            x: x,
+            c_a: c_a,
+        }
+    }
+
+    fn dfs(&self, idx: usize, asum: Vec<usize>) -> usize {
+        let mut res = std::usize::MAX;
+        if idx == self.n {
+            if (1..self.m+1).all(|i| asum[i] >= self.x) {
+                res = res.min(asum[0]);
+            }
+        } else {
+            // 次を選ばない場合
+            res = min(res, self.dfs(idx+1, asum.clone()));
+
+            // 次を選ぶ場合
+            let asum2 = asum.iter().zip(self.c_a[idx].iter())
+                                    .map(|(x, y)| x + y)
+                                    .collect::<Vec<usize>>();
+            res = min(res, self.dfs(idx + 1, asum2))
+
+        }
+
+        res
+    }
 }
