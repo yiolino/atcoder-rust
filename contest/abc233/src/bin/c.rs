8a390653c856bc9bd652890337f9e2fd6a5c92cb
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-07 08:33:29 +0900

    solved

--- contest/abc233/src/bin/c.rs
+++ contest/abc233/src/bin/c.rs
@@ -1,10 +1,9 @@
 #[allow(unused_imports)]
 use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
 #[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
+use std::collections::*;
 #[allow(unused_imports)]
 use std::cmp::{max, min, Reverse};
-use std::vec;
 #[allow(unused_imports)]
 use itertools::Itertools;
 #[allow(unused_imports)]
@@ -15,98 +14,30 @@ use superslice::Ext;
 fn main() {
     input!{
         n: usize,
-        x: usize,   
+        x: usize,
+        a: [[usize]; n]  // [usize] で受け取ると, 0idxが配列数となることに注意
     }
 
-    let mut l = vec![];
+    // BTreeMapで解いてみる
+    // 再起的かつdp的な考え方 xからaで割っていって、1に辿り着くかどうか
 
-    for _ in 0..n {
-        input! {li: usize, a: [usize; li]};
-        l.push(a);
-    }
+    let mut bmap = BTreeMap::new();
+    bmap.insert(x, 1_usize);
 
-    let mut dfs = DFS::new(n, x);
-
-    dfs.dfs(&l, 0, 1);
-
-    println!("{}", dfs.ans)
-}
-
-struct DFS {
-    ans: usize,
-    n: usize,
-    x: usize,
-}
-
-impl DFS {
-    fn new(n: usize, x: usize) -> Self {
-        DFS{ans: 0, n: n, x: x}
-    }
-
-    fn dfs(&mut self, l: &Vec<Vec<usize>>, i: usize, y: usize) {
-        if i == self.n {
-            if y == self.x {
-                self.ans += 1;
+    for a in a {
+        let mut next = BTreeMap::new();
+        for (x, w) in bmap {
+            for a in &a {
+                if x % *a == 0 {
+                    *next.entry(x / *a).or_insert(0) += w;
+                }
             }
-            return;
         }
 
-        for j in 0..l[i].len() {
-            if y > self.x / l[i][j] {
-                continue;
-            }
-            self.dfs(l, i + 1, y * l[i][j])
-        }
+        bmap = next;
     }
+
+    let ans = bmap.get(&1).map_or(0, |p| *p);
+
+    println!("{}", ans);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // DP で実装したが、どこかがあかん
-    // // xの約数の箇所だけカウントする手法でないとメモリ、計算量共に間に合わない。
-    // let mut yakusu = vec![];
-    // for i in 1..x {
-    //     if i * i > x {
-    //         break;
-    //     }
-    //     if x % i == 0 {
-    //         yakusu.push(i);
-    //         yakusu.push(x / i);
-    //     }
-    // }
-    // yakusu.sort(); // sortしておく
-
-    // // idxのlookupを作っておく
-    // let mut map = HashMap::new();
-    // for (i, y) in yakusu.iter().enumerate() {
-    //     map.insert(y, i);
-    // }
-
-
-    // let mut dp = vec![vec![0; yakusu.len()]; n + 1];
-
-    // dp[0][yakusu.len() - 1] = 1;
-
-    // for i in 0..n {
-    //     for j in (0..yakusu.len()).rev() {
-    //         for a in &l[i] {
-    //             if dp[i][j] != 0 && yakusu[j] % a == 0 {
-    //                 let idx = map.get(&(yakusu[j] / a)).unwrap();
-    //                 dp[i+1][*idx] += dp[i][j];
-    //             }
-    //         }
-            
-    //     }
-    // }
-
-    // println!("{}", dp[n][0]);
