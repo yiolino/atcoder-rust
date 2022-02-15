7a526f39315b3494c85310d06bda7b6b99db4281
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-16 06:46:48 +0900

    solved

--- contest/abc040/src/bin/b.rs
+++ contest/abc040/src/bin/b.rs
@@ -1,16 +1,35 @@
 #[allow(unused_imports)]
-use proconio::{input, fastout, marker::Chars};
+use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
 #[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet};
+use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
 #[allow(unused_imports)]
-use std::cmp::{max, min};
+use std::cmp::{max, min, Reverse};
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use superslice::Ext;
 
-#[fastout]
-#[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        n: i64,
     }
 
-    println!();
+    if n == 1 {
+        println!("0");
+        return;
+    }
+
+    let mut ans = std::i64::MAX;
+
+    for i in 1..n {
+        let y = n / i;
+        let amari = n - (n / i * i );
+
+        ans = ans.min((y - i).abs() + amari);
+    }
+
+
+    println!("{}", ans);
 }
