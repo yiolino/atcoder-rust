89bcba8b6f3602a776c23dbe6ea7c1809ed243ba
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-16 21:09:16 +0900

    solved

--- contest/abc123/src/bin/c.rs
+++ contest/abc123/src/bin/c.rs
@@ -1,3 +1,14 @@
+use proconio::*;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: usize,
+        a: [usize; 5],
+    }
+
+    let min = a.iter().min().unwrap();
+
+    let ans = (n + min - 1)/ min + 4;
+
+    println!("{}", ans);
 }
