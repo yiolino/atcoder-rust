7882c37edfe0d73174a67d148e42199b3e758513
tetsuro90 <hisayoshi0530@gmail.com>
2022-02-27 07:26:11 +0900

    solved

--- contest/abc065/src/bin/c.rs
+++ contest/abc065/src/bin/c.rs
@@ -1,18 +1,38 @@
-#[allow(unused_imports)]
-use proconio::{input, fastout, marker::Chars};
-#[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet, VecDeque};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use itertools::Itertools;
+use proconio::*;
 
-#[fastout]
-#[allow(non_snake_case)]
+const MOD: i64 = 1_000_000_007;
+
 fn main() {
     input!{
-        
+        mut n: i64,
+        mut m: i64,
     }
 
-    println!();
+    if m > n {
+        std::mem::swap(&mut n, &mut m);
+    }
+
+    if n - m > 1 {
+        println!("0");
+        return;
+    }
+
+    let mut ans = 1;
+
+    for i in 1..=n {
+        ans *= i;
+        ans %= MOD;
+    }
+
+    for i in 1..=m {
+        ans *= i;
+        ans %= MOD;
+    }
+
+    if n - m == 0 {
+        ans += ans;
+        ans %= MOD;
+    }
+
+    println!("{}", ans);
 }
