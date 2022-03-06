982e5165a49e041066e53a2a3c2eb6e75caf8777
tetsuro90 <hisayoshi0530@gmail.com>
2022-03-06 17:25:36 +0900

    solved

---
+++ contest/abc143/src/bin/d_hamayan.rs
@@ -0,0 +1,50 @@
+use proconio::input;
+
+fn main() {
+    input!{
+        n: usize,
+        l: [i64; n],
+    }
+
+    // 累積和で考える
+    let mut cum = vec![0; 2010];
+
+    for i in &l {
+        cum[*i as usize] += 1;
+    }
+
+    // cum[i] := [0, i) の積和
+    for i in 1..cum.len() {
+        cum[i] += cum[i - 1];
+    }
+
+    // a < b < c と考える -> これはだめで、a == b == c の場合を忘れている。
+    let mut ans = 0_usize;
+    for a in 0..n {
+        for b in 0..n {
+            if a == b {
+                continue;
+            }
+            let min = (l[a] - l[b]).max(l[b] - l[a]);
+            let max = l[a] + l[b];
+
+            let mut cnt = 0;
+            if 0 <= max - 1 {
+                cnt = cum[(max - 1) as usize];
+            }
+            cnt -= cum[min as usize];
+
+            if min < l[a] && l[a] < max {
+                cnt -= 1;
+            }
+            if min < l[b] && l[b] < max {
+                cnt -= 1;
+            }
+
+            ans += cnt;
+        }
+    }
+
+    ans /= 6;
+    println!("{}", ans);
+}
