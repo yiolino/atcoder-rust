2f4e16732407322eb2f157581667bd15dfaead0a
tetsuro90 <hisayoshi0530@gmail.com>
2022-03-08 06:58:39 +0900

    sovled

--- contest/abc122/src/bin/c.rs
+++ contest/abc122/src/bin/c.rs
@@ -1,16 +1,32 @@
-#[allow(unused_imports)]
-use proconio::{input, fastout, marker::Chars};
-#[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
+use proconio::{input, marker::{Chars, Usize1}};
 
-#[fastout]
-#[allow(non_snake_case)]
 fn main() {
     input!{
-        
+        n:usize,
+        q: usize,
+        s: Chars,
     }
 
-    println!();
+    let mut cum = vec![0; n + 1];
+    let mut mark_bet = vec![0; n + 1];  // AとCの間のチェック;
+    for (i, w) in s.windows(2).enumerate() {
+        if w[0] == 'A' && w[1] == 'C' {
+            mark_bet[i + 1] = 1;
+            cum[i + 2] = 1;
+        }
+    }
+
+    for i in 1..cum.len() {
+        cum[i] += cum[i - 1]
+    }
+
+    for _ in 0..q {
+        input! {l: Usize1, r: Usize1};
+        let mut ans: usize = cum[r + 1] - cum[l];
+        if mark_bet[l] == 1 {
+            ans -= 1;
+        }
+
+        println!("{}", ans)
+    }
 }
