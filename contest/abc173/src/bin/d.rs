35e31f486172774fdc7aaf924c94fa8fc5608668
tetsuro90 <hisayoshi0530@gmail.com>
2022-03-16 08:14:54 +0900

    solved

--- contest/abc173/src/bin/d.rs
+++ contest/abc173/src/bin/d.rs
@@ -1,20 +1,29 @@
-#[allow(unused_imports)]
-use proconio::{input, fastout, marker::Chars, marker::Usize1, marker::Bytes};
-#[allow(unused_imports)]
-use std::collections::{HashSet, HashMap, BTreeSet, VecDeque, BinaryHeap};
-#[allow(unused_imports)]
-use std::cmp::{max, min, Reverse};
-#[allow(unused_imports)]
-use itertools::Itertools;
-#[allow(unused_imports)]
-use petgraph::unionfind::UnionFind;
-#[allow(unused_imports)]
-use superslice::Ext;
+use std::{cmp::Reverse};
+use proconio::input;
+
 
 fn main() {
     input!{
-        
+        n:usize,
+        mut a: [usize; n],
     }
 
-    println!();
+    a.sort_by_key(|x| Reverse(*x));
+
+    let mut ans = 0;
+
+    let mut t = n - 1;
+    for i in 0..n {
+        let mut limit = 2;
+        if i == 0 {
+            limit = 1;
+        }
+        for _ in 0..limit {
+            if t > 0 {
+                ans += a[i];
+                t -= 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
