00b4d1a9a0c1685a0fda7e63412868dc5e644625
tetsuro90 <hisayoshi0530@gmail.com>
2022-03-26 07:04:42 +0900

    solved

--- contest/arc129/src/bin/b.rs
+++ contest/arc129/src/bin/b.rs
@@ -2,8 +2,25 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
     }
 
-    println!();
+    let mut a = 0_i64;
+    let mut b = std::i64::MAX;
+
+    for _ in 0..n {
+        input! {l: i64, r: i64};
+        a = a.max(l);
+        b = b.min(r);
+
+        let ans = if a <= b {
+            0
+        } else {
+            {
+                ((a - b) + 1)/ 2
+            }
+        };
+
+        println!("{}", ans);
+    }
 }
