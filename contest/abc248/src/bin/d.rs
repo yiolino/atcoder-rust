2bb5fa84f7cb47715b6cca6fb7e38770021b8bc5
tetsuro90 <hisayoshi0530@gmail.com>
2022-06-11 09:54:05 +0900

    solved abc248 d

--- contest/abc248/src/bin/d.rs
+++ contest/abc248/src/bin/d.rs
@@ -1,9 +1,24 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n],
+        q: usize,
     }
 
-    println!();
+    // x毎にindexを保存しておき、二分探索を実施する。
+    let mut index_mat = vec![vec![]; n+1];
+    for (i, ai) in a.iter().enumerate() {
+        index_mat[*ai].push(i);
+    }
+
+    for _ in 0..q {
+        input! {l: usize, r: usize, x: usize};
+        let lower = index_mat[x].lower_bound(&(l - 1));
+        let upper = index_mat[x].lower_bound(&r);
+
+        println!("{}", upper - lower);
+    }
 }
