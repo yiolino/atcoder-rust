5ffce46208e7839e11115730fb7a8092d6dee1d3
tetsuro90 <hisayoshi0530@gmail.com>
2022-07-16 06:50:31 +0900

    solved abc258 d

--- contest/abc258/src/bin/d.rs
+++ contest/abc258/src/bin/d.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        x: usize,
+        ab: [(usize, usize); n],
     }
 
-    println!();
+    let mut ans = std::usize::MAX;
+    let mut head = 0;
+    
+    for i in 0..n {
+        head += ab[i].0;
+        let tmp = (x - i) * ab[i].1 + head;
+        ans = ans.min(tmp);
+
+        head += ab[i].1;
+    }
+
+    println!("{}", ans);
 }
