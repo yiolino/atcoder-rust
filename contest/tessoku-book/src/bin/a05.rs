386bba6aa35ed6015cd6b6cf2b1597b5356809b6
tetsuro90 <hisayoshi0530@gmail.com>
2022-10-17 12:47:54 +0900

    solved

--- contest/tessoku-book/src/bin/a05.rs
+++ contest/tessoku-book/src/bin/a05.rs
@@ -2,8 +2,18 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: i32,
+        k: i32,
     }
 
-    println!();
+    let mut ans = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+            if k - i - j >= 1 && k - i - j <= n {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
