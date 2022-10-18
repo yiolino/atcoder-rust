a4ef2fe0f0712fb21d82b7ee0433c7002c3afec3
tetsuro90 <hisayoshi0530@gmail.com>
2022-10-18 13:36:54 +0900

    solved a06

--- contest/tessoku-book/src/bin/a04.rs
+++ contest/tessoku-book/src/bin/a04.rs
@@ -1,6 +1,3 @@
-use std::collections::VecDeque;
-
-use itertools::Itertools;
 use proconio::input;
 
 fn main() {
--- contest/tessoku-book/src/bin/a06.rs
+++ contest/tessoku-book/src/bin/a06.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        q: usize,
+        a: [usize; n],
     }
 
-    println!();
+    let mut cum = vec![0; n + 1];
+    for (i, ai) in a.iter().enumerate() {
+        cum[i + 1] = cum[i] + ai;
+    }
+
+    for _ in 0..q {
+        input! {l: usize, q: usize};
+        
+        let ans = cum[q] - cum[l - 1];
+        println!("{}", ans);
+    }
 }
