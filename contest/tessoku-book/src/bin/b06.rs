5ff1f1ec31ce5a8fd07a8dda35df4943b612f2c8
tetsuro90 <hisayoshi0530@gmail.com>
2022-10-19 12:57:54 +0900

    solved b06

--- contest/tessoku-book/src/bin/b06.rs
+++ contest/tessoku-book/src/bin/b06.rs
@@ -2,8 +2,31 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n],
+        q: usize,
     }
 
-    println!();
+    let mut cum = vec![0; n + 1];
+    for (i, ai) in a.iter().enumerate() {
+        if *ai == 1 {
+            cum[i + 1] = cum[i] + 1;
+        } else {
+            cum[i + 1] = cum[i]
+        }
+    }
+
+    for _ in 0..q {
+        input! {l: usize, r: usize};
+        let num_atari = cum[r] - cum[l - 1];
+        let ans = if num_atari > r - l + 1 - num_atari {
+            "win"
+        } else if num_atari == r - l + 1 - num_atari {
+            "draw"
+        } else {
+            "lose"
+        };
+
+        println!("{}", ans);
+    }
 }
