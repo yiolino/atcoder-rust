5a73b774d74a453a83a236ea4f2727632602cd6f
tetsuro90 <hisayoshi0530@gmail.com>
2022-10-23 20:53:48 +0900

    solved a07

--- contest/tessoku-book/src/bin/a07.rs
+++ contest/tessoku-book/src/bin/a07.rs
@@ -2,8 +2,23 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        d: usize,
+        n: usize,
     }
 
-    println!();
+    let mut cum = vec![0; d + 2];
+
+    for _ in 0..n {
+        input! {l: usize, r: usize};
+        cum[l] += 1;
+        cum[r + 1] -= 1;
+    }
+
+    for i in 1..(d + 2) {
+        cum[i] = cum[i] + cum[i - 1];
+    }
+
+    for i in 1..d+1 {
+        println!("{}", cum[i])
+    }
 }
