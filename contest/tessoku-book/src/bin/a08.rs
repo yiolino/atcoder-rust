324cb4410ae71d29c3b85bf8c15964076eeb086a
tetsuro90 <hisayoshi0530@gmail.com>
2022-10-31 23:18:29 +0900

    solved a08

--- contest/tessoku-book/src/bin/a08.rs
+++ contest/tessoku-book/src/bin/a08.rs
@@ -1,9 +1,37 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input!{
-        
+        h: usize,
+        w: usize,
+        mut x: [[usize; w]; h],
+        q: usize,
     }
 
-    println!();
+    // 横方向の累積
+    for i in 0..h {
+        for j in 1..w {
+            x[i][j] += x[i][j - 1];
+        }
+    }
+
+    // 縦方向の累積
+    for i in 0..w {
+        for j in 1..h {
+            x[j][i] += x[j - 1][i];
+        }
+    }
+
+    for i in 0..h {
+        x[i].insert(0, 0);
+    }
+
+    x.insert(0, vec![0; w + 1]);
+
+    for _ in 0..q {
+        input! {a: usize, b: usize, c: usize, d: usize};
+
+        let ans = x[c][d] + x[a - 1][b - 1] - x[c][b - 1] - x[a - 1][d];
+        println!("{}", ans)
+    }
 }
