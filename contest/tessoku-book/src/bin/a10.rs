708c10a41257acd1a875e740003c5bed3c01cd75
tetsuro90 <hisayoshi0530@gmail.com>
2022-11-22 13:37:22 +0900

    solved a10

--- contest/tessoku-book/src/bin/a10.rs
+++ contest/tessoku-book/src/bin/a10.rs
@@ -1,9 +1,29 @@
-use proconio::input;
+use std::cmp::max;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n],
+        d: usize,
     }
 
-    println!();
+    let mut p = vec![0; n];
+    p[0] = a[0];
+    for i in 1..n {
+        p[i] = a[i].max(p[i - 1]);
+    }
+
+    let mut q = vec![0; n];
+    q[n-1] = a[n-1];
+    for i in (0..n-1).rev() {
+        q[i] = a[i].max(q[i + 1]);
+    }
+
+    for _ in 0..d {
+        input! {l: Usize1, r: Usize1};
+
+        let ans = max(p[l - 1], q[r + 1]);
+        println!("{}", ans);
+    }
 }
