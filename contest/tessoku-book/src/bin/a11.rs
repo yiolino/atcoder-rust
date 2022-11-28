c09342c3065679e6ddc586daede297155466a3cb
tetsuro90 <hisayoshi0530@gmail.com>
2022-11-28 12:56:19 +0900

    solved a11

--- contest/tessoku-book/src/bin/a11.rs
+++ contest/tessoku-book/src/bin/a11.rs
@@ -2,8 +2,23 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        x: usize,
+        mut a: [usize; n]
     }
 
-    println!();
+    a.sort();
+
+    let mut lower = 0;
+    let mut upper = n - 1;
+    while upper - lower > 1 {
+        let mid = (upper + lower) / 2;
+        if a[mid] >= x {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+
+    println!("{}", upper + 1);
 }
