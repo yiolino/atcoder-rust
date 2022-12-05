f67edc84577dc71af9bf74aee2fff44ac35a2721
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-05 13:07:37 +0900

    solved a12

--- contest/tessoku-book/src/bin/a12.rs
+++ contest/tessoku-book/src/bin/a12.rs
@@ -2,8 +2,36 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        k: usize,
+        a: [usize; n],   
     }
 
-    println!();
+    let mut upper = 1000_000_000;
+    let mut lower = 0;
+
+    while upper - lower > 1 {
+        let mid = (upper + lower) / 2;
+        if check(mid, k, &a) {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+
+    println!("{}", upper);
+}
+
+// k枚印刷するのにx秒以内ならtrue
+fn check(x: usize, k: usize, a: &Vec<usize>) -> bool {
+    let mut p = 0;
+    for ai in a {
+        p += x / ai;
+    }
+
+    if p >= k {
+        true
+    } else {
+        false
+    }
 }
