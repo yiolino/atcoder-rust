15d8187e2e00aab35172d00311a1246b08a82c70
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-12 12:59:07 +0900

    solved b12

--- contest/tessoku-book/src/bin/b12.rs
+++ contest/tessoku-book/src/bin/b12.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: f64,
     }
 
-    println!();
+    let mut upper = std::f64::MAX;
+    let mut lower = 0.0;
+
+    while upper - lower > 0.0001 {
+        let mid = (upper + lower) / 2.0;
+        let tmp = f(mid);
+        if tmp > n {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+
+    println!("{}", upper)
+}
+
+
+fn f(x: f64) -> f64 {
+    x * x * x + x
 }
