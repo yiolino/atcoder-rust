bb99fda86a157c3717fe6532ffe5ee973d526534
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-14 23:29:22 +0900

    solved a13.rs

--- contest/tessoku-book/src/bin/a13.rs
+++ contest/tessoku-book/src/bin/a13.rs
@@ -2,8 +2,29 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        k: usize,
+        a: [usize; n],
     }
 
-    println!();
+    let mut r = vec![0; n];
+
+    for i in 0..n {
+        if i == 0 {
+            r[i] = 0;
+        } else {
+            r[i] = r[i - 1];
+        }
+
+        while r[i] < n - 1 && a[r[i] + 1] - a[i] <= k {
+            r[i] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += r[i] - i;
+    }
+
+    println!("{}", ans);
 }
