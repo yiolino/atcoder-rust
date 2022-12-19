0ea3b779a96faec8792160ce5298ba3ecd2e64e7
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-19 13:13:53 +0900

    solved b13

--- contest/tessoku-book/src/bin/b13.rs
+++ contest/tessoku-book/src/bin/b13.rs
@@ -2,8 +2,35 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        k: usize,
+        a: [usize; n]
     }
 
-    println!();
+    // 累積和を求める
+    let mut cum = vec![0; n + 1];
+    for i in 1..n+1 {
+        cum[i] = cum[i - 1] + a[i - 1]
+    }
+
+    let mut r = vec![0; n];
+
+    for i in 0..n {
+        if i == 0 {
+            r[i] = 0;
+        } else {
+            r[i] = r[i - 1];
+        }
+
+        while r[i] < n && cum[r[i] + 1] - cum[i] <= k {
+            r[i] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += r[i] - i;
+    }
+
+    println!("{}", ans);
 }
