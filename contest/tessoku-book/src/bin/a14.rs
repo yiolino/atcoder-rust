dd4623d76c55f7918ce1aa430462ecf40f3a09d1
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-20 13:45:30 +0900

    solvedd a14

--- contest/tessoku-book/src/bin/a14.rs
+++ contest/tessoku-book/src/bin/a14.rs
@@ -1,9 +1,40 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input!{
-        
+        n: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
+        d: [usize; n],
     }
 
-    println!();
+    let mut p = vec![0; n * n];
+    let mut q = vec![0; n * n];
+
+    for (i, ai) in a.iter().enumerate() {
+        for (j, bj) in b.iter().enumerate() {
+            p[i * n + j] = ai + bj;
+        }
+    }
+
+    for (i, ci) in c.iter().enumerate() {
+        for (j, dj) in d.iter().enumerate() {
+            q[i * n + j] = ci + dj;
+        }
+    }
+
+    q.sort();
+
+    for pi in p {
+        let idx = q.lower_bound(&(k - pi));
+        if idx < n *n && q[idx] == k - pi {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
