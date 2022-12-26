224cd581e25d3b6d5a1558dc3abb773af9303e23
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-26 13:10:10 +0900

    solved a15

--- contest/tessoku-book/src/bin/a15.rs
+++ contest/tessoku-book/src/bin/a15.rs
@@ -1,9 +1,24 @@
+use itertools::Itertools;
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input!{
-        
+        n: usize,
+        a: [usize; n],
     }
 
-    println!();
+    let mut z = a.clone();
+    z.sort();
+    z.dedup();
+
+    let mut b = vec![];
+    
+    for ai in a {
+        let bi = z.lower_bound(&ai);
+        b.push(bi + 1);
+    }
+
+    let ans = b.into_iter().join(" ");
+    println!("{}", ans)
 }
