7d4eff003b76b715b2730f866ce8f77b400fd027
tetsuro90 <hisayoshi0530@gmail.com>
2022-12-28 12:41:14 +0900

    solved b16

--- contest/tessoku-book/src/bin/b16.rs
+++ contest/tessoku-book/src/bin/b16.rs
@@ -1,9 +1,21 @@
+use std::cmp::min;
 use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        h: [i32; n],
     }
 
-    println!();
+    let mut dp = vec![0; n];
+    dp[1] = (h[1] - h[0]).abs();
+    
+    for i in 2..n {
+        dp[i] = min(
+            dp[i - 1] + (h[i] - h[i - 1]).abs(),
+            dp[i - 2] + (h[i] - h[i - 2]).abs(),
+        );
+    }
+
+    println!("{}", dp[n - 1]);
 }
