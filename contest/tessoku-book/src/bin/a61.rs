5eee02d3cc88a4d5223873438e4c5563737709f0
tetsuro90 <hisayoshi0530@gmail.com>
2023-01-11 13:29:55 +0900

    solved a61

--- contest/tessoku-book/src/bin/a61.rs
+++ contest/tessoku-book/src/bin/a61.rs
@@ -1,9 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
 
-    println!();
+    let mut graph = vec![vec![]; 100000 + 10];
+    for (a, b) in ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    for i in 1..=n {
+        print!("{}: {{", i);
+        print!("{}", graph[i].iter().join(", "));
+        println!("}}");
+    }
 }
