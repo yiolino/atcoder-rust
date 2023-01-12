6474a9a046806ef261ea1b615ab47d34a9c83c5c
tetsuro90 <hisayoshi0530@gmail.com>
2023-01-12 13:03:56 +0900

    solved b61.rs

--- contest/tessoku-book/src/bin/b61.rs
+++ contest/tessoku-book/src/bin/b61.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
 
-    println!();
+    let mut graph = vec![vec![]; 100000 + 10];
+
+    for (a, b) in ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut max = 0;
+    let mut ans = 0;
+    for (i, node) in graph.into_iter().enumerate() {
+        if node.len() >= max {
+            max = node.len();
+            ans = i;
+        }
+    }
+
+    println!("{}", ans);
 }
