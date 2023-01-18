2c30a1082bfa1bc1d199d715676dfaab6fd8f743
tetsuro90 <hisayoshi0530@gmail.com>
2023-01-18 13:26:06 +0900

    solved a62

--- contest/tessoku-book/src/bin/a62.rs
+++ contest/tessoku-book/src/bin/a62.rs
@@ -1,9 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input!{
-        
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     }
 
-    println!();
+    let mut graph = vec![vec![]; n];
+    for (a, b) in ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut seen = vec![false; n];
+    dfs(&graph, 0, &mut seen);
+
+    if seen.iter().all(|x| *x) {
+        println!("The graph is connected.")
+    } else {
+        println!("The graph is not connected.")
+    }
+}
+
+fn dfs(graph: &Vec<Vec<usize>>, start: usize, seen: &mut Vec<bool>) {
+    if !seen[start] {
+        seen[start] = true;
+        for next in &graph[start] {
+            dfs(graph, *next, seen);
+        }
+    }
 }
