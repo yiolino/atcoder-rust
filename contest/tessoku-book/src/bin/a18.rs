f75250ff7b546a465e6313fdb92e5fbb59136e95
tetsuro90 <hisayoshi0530@gmail.com>
2024-06-05 13:13:52 +0900

    solved a18

--- contest/tessoku-book/src/bin/a18.rs
+++ contest/tessoku-book/src/bin/a18.rs
@@ -1,9 +1,30 @@
 use proconio::input;
 
 fn main() {
-    input!{
-        
+    // dp二次元配列を考える
+    // dp[i][j] = カード1 ~ iの中から何枚かを選び、選んだカードに書かれた整数の合計をjにすることは可能か
+    input! {
+        n:  usize,
+        s:  usize,
+        a:  [usize; n],
     }
 
-    println!();
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for i in 1..(n + 1) {
+        for j in 0..(s + 1) {
+            if dp[i - 1][j] {
+                dp[i][j] = dp[i - 1][j]
+            }
+            let ai = a[i - 1];
+            if j >= ai && dp[i - 1][j - ai] {
+                dp[i][j] = dp[i - 1][j - ai]
+            }
+        }
+    }
+
+    let ans = if dp[n][s] { "Yes" } else { "No" };
+
+    println!("{}", ans);
 }
--- contest/tessoku-book/src/bin/b62.rs
+++ contest/tessoku-book/src/bin/b62.rs
@@ -1,9 +1,42 @@
+use std::collections::VecDeque;
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input!{
-        
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
 
-    println!();
+    let mut graph = vec![vec![]; n + 1];
+    for (a, b) in ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut seen = vec![false; n + 1];
+    let mut stack = VecDeque::new();
+
+    dfs(1, &graph, &mut seen, &mut stack);
+
+    let ans = stack.iter().join(" ");
+    println!("{}", ans);
+}
+
+fn dfs(node: usize, graph: &Vec<Vec<usize>>, seen: &mut Vec<bool>, stack: &mut VecDeque<usize>) {
+    let goal = graph.len() + 1;
+    if node == goal {
+        return;
+    }
+
+    seen[node] = true;
+    stack.push_back(node);
+    for next in &graph[node] {
+        if !seen[*next] {
+            dfs(*next, graph, seen, stack);
+        }
+    }
+
+    stack.pop_back();
 }
